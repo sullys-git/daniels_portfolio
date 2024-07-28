1d1580cb85eb9dbfb301eccbf69cdee94ae9b707
sullys-git <157181686+sullys-git@users.noreply.github.com>
2024-07-27 21:31:21 -0600

    portfolio section updated

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let navWrapper = document.getElementById("nav-wrapper");
-let navHTML = '<div class="nav-items"><ul><li><a href="index.html">Home</a></li><li><a href="#about-section">About</a></li><li><a href="resume.html">Resume</resume></li><li><a href="portfolio.html">Portfolio</a></li><li><a href="#contact-section">Contact</a></li></ul></div>'
+let navHTML = '<div class="nav-items"><ul><li><a href="index.html">Home</a></li><li><a href="#about-section">About</a></li><li><a href="resume.html">Resume</resume></li><li><a href="#portfolio-section">Portfolio</a></li><li><a href="#contact-section">Contact</a></li></ul></div>'
 navWrapper.innerHTML = navHTML;
 
 
